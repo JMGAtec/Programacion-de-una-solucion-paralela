3064619d9386cb12875280b86e87e2340fce3ee0
206521077 <206521077@MXMXCF4J9TT3>
2024-01-28 21:31:23 -0600

    Add project files.

---
+++ openMP_test/pch.h
@@ -0,0 +1,9 @@
+// pch.h: este es un archivo de encabezado precompilado.
+#ifndef PCH_H
+#define PCH_H
+
+// Agrega aquí los encabezados que quieres precompilar.
+#include <iostream>
+
+#endif //PCH_H
+
